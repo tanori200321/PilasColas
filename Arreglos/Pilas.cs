8983fd4dc084678b45e8c2da9ad90cc1fe1da7c8
SebastianGtr <tanoriruizs@gmail.com>
2022-03-26 12:11:45 -0700

    Pilas

---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Pilas
+    {
+        private string[] array;
+        private int max;
+        private int tope;
+        public Pilas(int tamaño)
+        {
+            array = new string[tamaño];
+            max = array.Length-1;
+            tope = 0;        
+        }
+
+    }
+
+}
