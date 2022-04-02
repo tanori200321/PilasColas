b8ddcc9a63b7372c932c390bfc94e53d1842c9a1
SebastianGtr <tanoriruizs@gmail.com>
2022-04-02 13:36:45 -0700

    Cambios

--- Listas/Pilas.cs
+++ Listas/Pilas.cs
@@ -8,5 +8,48 @@ namespace Listas
 {
     public class Pilas
     {
+        private List<string> list;
+        public Pilas()
+        {
+            list = new List<string>();
+        }
+        public void Agregar(string dato)
+        {
+            list.Add(dato);
+        }
+        private bool ValidarVacio()
+        {
+            return list.Count > 0;
+        }
+        public void Eliminar()
+        {
+            if (ValidarVacio())
+            {
+                throw new Exception("Lista Vacia");
+            }
+            list.RemoveAt(list.Count -1);
+        }
+        public string Imprimir()
+        {
+            string datos = "";
+
+            if (ValidarVacio())
+            {
+                return "Lista Vacia";
+
+            }
+            int lstCount = list.Count;
+            for (int i = 0; i < lstCount; i++)
+            {
+                if (i > 0)
+                {
+                    datos = "\n";
+                }
+                datos += $"[{i}] - {list[i]}";
+            }
+            return datos;
+
+        }
+
     }
 }
--- PilasColas/Program.cs
+++ PilasColas/Program.cs
@@ -1,31 +1,31 @@
-﻿using Arreglos;
-using Listas;
+﻿//using Arreglos;
+//using Listas;
 
-Listas.Pilas pilas = new Listas.Pilas(10);
-pilas.Agregar("cero");
-pilas.Agregar("uno");
-pilas.Agregar("dos");
-pilas.Agregar("tres");
-pilas.Agregar("cuatro");
-pilas.Agregar("cinco");
-Console.WriteLine(pilas.Imprimir());
+//Listas.Pilas pilas = new Listas.Pilas(10);
+//pilas.Agregar("cero");
+//pilas.Agregar("uno");
+//pilas.Agregar("dos");
+//pilas.Agregar("tres");
+//pilas.Agregar("cuatro");
+//pilas.Agregar("cinco");
+//Console.WriteLine(pilas.Imprimir());
 
-pilas.Eliminar();
-Console.WriteLine(pilas.Imprimir());
+//pilas.Eliminar();
+//Console.WriteLine(pilas.Imprimir());
 
-for (int i = 0; i < 10; i++)
-{
-    try
-    {
-        Console.WriteLine("---------------");
-        pilas.Eliminar();
-        Console.WriteLine(pilas.Imprimir());
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-}
+//for (int i = 0; i < 10; i++)
+//{
+//    try
+//    {
+//        Console.WriteLine("---------------");
+//        pilas.Eliminar();
+//        Console.WriteLine(pilas.Imprimir());
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine(ex.Message);
+//    }
+//}
 Listas.Pilas listas = new Listas.Pilas();
 
 
